94f010602d1d13069cc5193194e8d71a259b3299
buyi <huaqirong@126.com>
2016-09-28 15:37:27 +0800

    init project

---
+++ buildSrc/src/main/groovy/GroovinPlugin.groovy
@@ -0,0 +1,14 @@
+import org.gradle.api.*
+import org.gradle.api.plugins.*
+class GroovinPlugin implements Plugin<Project> {
+	void apply (Project project) {
+		project.extensions.create("groovin", GroovinPluginExtension)
+		  project.task('groove') << {
+            println project.groovin.message
+        }
+	}
+
+}
+class GroovinPluginExtension {
+    def String message = 'We be groovin\''
+}
